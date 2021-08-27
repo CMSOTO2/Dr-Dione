8238e6b5f940a2954f2eb980438e4a7c155b5eff
CMSOTO2 <81086284+CMSOTO2@users.noreply.github.com>
2021-08-26 22:13:03 -0400

    Added Scrolling Text
    slick slider scrolling text

--- app.js
+++ app.js
@@ -1,52 +1,24 @@
-// $(document).ready(function () {
-//   console.log("hi");
-//   $(".text-scroll").slick({
-//     arrows: true,
-//     slidesToShow: 5,
-//     slidesToScroll: 1,
-//     autoplay: true,
-//     autoplaySpeed: 0,
-//     speed: 3000,
-//     pauseOnHover: false,
-//     cssEase: "linear",
-//     responsive: [
-//       {
-//         breakpoint: 1200,
-//         settings: {
-//           slidesToShow: 5,
-//         },
-//       },
-//       {
-//         breakpoint: 768,
-//         settings: {
-//           slidesToShow: 3,
-//         },
-//       },
-//     ],
-//   });
-//   $(".right-logo-slider").slick({
-//     arrows: false,
-//     slidesToShow: 5,
-//     slidesToScroll: 1,
-//     autoplay: true,
-//     autoplaySpeed: 0,
-//     speed: 3000,
-//     pauseOnHover: false,
-//     cssEase: "linear",
-//     rtl: true,
-//     responsive: [
-//       {
-//         breakpoint: 1200,
-//         settings: {
-//           slidesToShow: 4,
-//         },
-//       },
-//       {
-//         breakpoint: 768,
-//         settings: {
-//           slidesToShow: 3,
-//         },
-//       },
-//     ],
-//   });
-// });
+$(".slick").slick({
+  arrows: false,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 0,
+  speed: 3000,
+  pauseOnHover: false,
+  cssEase: "linear",
+  responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 4,
+      },
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 3,
+      },
+    },
+  ],
+});
